5043693b6e371b9c16450f6f28051357ddf221ff
trinziknaxuy <itz9tee9@gmail.com>
2025-09-27 13:25:13 +0200

    map

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,15 +2,10 @@ extends Node2D
 
 @onready var player: CharacterBody2D = %Player
 
-const ENEMY = preload("uid://bgdracsuhgysh")
-
 func create_sword_item() -> ItemResource:
 	var sword_texture = preload("res://assets/wooden_sword.png")
 	return ItemResource.new("Wooden Sword", sword_texture, "An weak wooden sword", 1, "weapon")
 	
 func _ready() -> void:
 	var sword = create_sword_item()
-	sword.damage = 10
 	player.add_item(sword)
-	
-	var enemy = ENEMY.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
