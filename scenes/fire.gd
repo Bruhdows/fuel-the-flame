e4242e6e4535c212c00e523f0b749d26c581f12a
Bruhdows <bruhdows@gmail.com>
2025-09-27 08:47:37 +0200

    Player and Fire models

---
+++ scenes/fire.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+@onready var fire_progress: ProgressBar = %FireProgress
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	fire_progress.value -= 1
