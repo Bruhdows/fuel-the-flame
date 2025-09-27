a443ab51272a5850c44531f0e304bd4c08fe8a87
LonleySailor <akacprzak@protonmail.com>
2025-09-27 11:41:49 +0200

    Improved fire bar

--- scenes/fire.gd
+++ scenes/fire.gd
@@ -1,16 +1,15 @@
 extends StaticBody2D
 
-@onready var fire_progress: ProgressBar = %FireProgress
+# Reference the label by its actual name
+@onready var fire_value: Label = $FireProgress
 
-# Called when the node enters the scene tree for the first time.
+var current_value: int = 150  # starting value
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	fire_value.text = str(current_value)
 
 func _on_timer_timeout() -> void:
-	fire_progress.value -= 1
+	current_value -= 1
+	if current_value < 0:
+		current_value = 0
+	fire_value.text = str(current_value)
