a3f25b6ca6c1f0d3e8fb6079abc83aed8799b259
LonleySailor <akacprzak@protonmail.com>
2025-09-27 14:37:00 +0200

    Game ending screen

--- scripts/fire.gd
+++ scripts/fire.gd
@@ -3,13 +3,22 @@ extends StaticBody2D
 # Reference the label by its actual name
 @onready var fire_value: Label = $FireProgress
 
-var current_value: int = 150  # starting value
+var current_value: int = 10  # starting value
 
 func _ready() -> void:
 	fire_value.text = str(current_value)
 
 func _on_timer_timeout() -> void:
 	current_value -= 1
-	if current_value < 0:
-		current_value = 0
 	fire_value.text = str(current_value)
+
+	if current_value <= 0:
+		# Stop the timer
+		$Timer.stop()
+		# Defer the scene change safely
+		call_deferred("change_to_game_ending")
+
+
+func change_to_game_ending() -> void:
+	# Godot 4 method for changing scene
+	get_tree().change_scene_to_file("res://scenes/game_ending.tscn")
